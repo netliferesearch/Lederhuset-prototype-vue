9c371a2124a445a959d8189e4ca94e4d600d4437
Matslande <matslande@gmail.com>
2018-03-15 11:30:41 +0100

    getting data from data.json and added theme color for each page

--- src/components/ThemeSite.vue
+++ src/components/ThemeSite.vue
@@ -1,23 +1,29 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{data.title}}</h1>
 
   </div>
 </template>
 
 <script>
 /* eslint-disable */
+import data from "@/assets/data/data.json";
 export default {
   name: "ThemeSite",
   data() {
     return {
-      msg: "Her er vi inne på en tema side"
+      data
     };
+  },
+  beforeCreate: function() {
+    document.body.className = "dark";
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-
+h1 {
+  color: white;
+}
 </style>
