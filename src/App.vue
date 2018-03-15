bd810a8a12a99513fdba8c53ab47dc9409933aa7
Matslande <matslande@gmail.com>
2018-03-15 08:53:27 +0100

    added SCSS 💅🏻

--- src/App.vue
+++ src/App.vue
@@ -20,16 +20,6 @@ export default {
 };
 </script>
 
-<style>
-:root {
-  --primary-color: blue;
-}
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "assets/styles/main.scss";
 </style>
