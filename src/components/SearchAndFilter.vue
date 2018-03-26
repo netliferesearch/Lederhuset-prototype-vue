664bdc153a9a01bb4fe465ec9b36ddb4533cc614
Nils Norman Haukås <nils@thunki.com>
2018-03-26 15:43:58 +0200

    enable feature toggling of whether or not to show unmatched results

--- src/components/SearchAndFilter.vue
+++ src/components/SearchAndFilter.vue
@@ -28,7 +28,7 @@
 
     <!-- Here we show categories that were not matched, but greyed out -->
     <row :key="cat.id" v-for="cat in matchList"
-      v-if="search.length !== 0 && !categoryHasAnyMatch(cat)">
+      v-if="showUnmatched && search.length !== 0 && !categoryHasAnyMatch(cat)">
        <column sm="4" class="lh-unmatched-result">
          <h2>{{cat.categoryTitle}}</h2>
        </column>
@@ -124,8 +124,16 @@ export default {
   data() {
     return {
       search: "",
-      categorizedPosts
+      categorizedPosts,
+      showUnmatched: true
     };
+  },
+  created() {
+    if (window.location) {
+      if (location.hash.includes('version=hideUnmatched')) {
+        this.showUnmatched = false
+      }
+    }
   },
   computed: {
     matchList() {
